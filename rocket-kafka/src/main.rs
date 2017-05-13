99276b2dfab2b18f5edcf090bf7287f3fd57ecd8
Jean LELIEVRE <Jean.LELIEVRE@supinfo.com>
2017-05-13 23:09:59 +0200

    integrate basic code

---
+++ rocket-kafka/src/main.rs
@@ -0,0 +1,17 @@
+#![feature(plugin)]
+#![plugin(rocket_codegen)]
+
+extern crate rocket;
+
+#[get("/ping")]
+fn ping() -> &'static str {
+    "OK"
+}
+
+fn main() {
+
+    rocket::ignite().mount(
+        "/api/1",
+        routes![ping]
+    ).launch();
+}
