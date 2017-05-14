d5940d0579603ffb77271f4537b8e2334c53eca6
Jean LELIEVRE <Jean.LELIEVRE@supinfo.com>
2017-05-14 21:49:32 +0200

    load producer from main script

--- rocket-kafka/src/main.rs
+++ rocket-kafka/src/main.rs
@@ -2,6 +2,11 @@
 #![plugin(rocket_codegen)]
 
 extern crate rocket;
+extern crate kafka;
+
+use std::time::Duration;
+
+use kafka::producer::{Producer, RequiredAcks};
 
 #[get("/ping")]
 fn ping() -> &'static str {
@@ -9,6 +14,13 @@ fn ping() -> &'static str {
 }
 
 fn main() {
+
+    let kafka_server = "rocket-kafka_kafka:9092";
+
+    let producer = Producer::from_hosts(vec![kafka_server.to_owned()])
+        .with_ack_timeout(Duration::from_secs(5))
+        .with_required_acks(RequiredAcks::One)
+        .create();
 
     rocket::ignite().mount(
         "/api/1",
