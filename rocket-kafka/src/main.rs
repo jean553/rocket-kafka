644a9845cf3641e569d6e656f301e497958f90f8
Jean LELIEVRE <Jean.LELIEVRE@supinfo.com>
2017-05-15 13:40:07 +0200

    send message to producer

--- rocket-kafka/src/main.rs
+++ rocket-kafka/src/main.rs
@@ -6,24 +6,28 @@ extern crate kafka;
 
 use std::time::Duration;
 
-use kafka::producer::{Producer, RequiredAcks};
+use kafka::producer::{Producer, Record, RequiredAcks};
 
-#[get("/ping")]
-fn ping() -> &'static str {
-    "OK"
-}
-
-fn main() {
+#[post("/message/<message>")]
+fn message(message: &str) -> &'static str {
 
     let kafka_server = "rocket-kafka_kafka:9092";
+    let topic = "topic";
 
     let producer = Producer::from_hosts(vec![kafka_server.to_owned()])
         .with_ack_timeout(Duration::from_secs(5))
         .with_required_acks(RequiredAcks::One)
         .create();
 
+    producer.send(&Record::from_value(topic, message));
+
+    return "OK";
+}
+
+fn main() {
+
     rocket::ignite().mount(
         "/api/1",
-        routes![ping]
+        routes![message]
     ).launch();
 }
